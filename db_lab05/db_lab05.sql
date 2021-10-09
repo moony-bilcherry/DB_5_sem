0928ead74c1b1f1ad5a61048cf6d60f9f17930cd
moony-bilcherry <erchinskaya.nataliya@gmail.com>
2021-10-09 09:05:36 +0300

    5 done

--- db_lab05/db_lab05.sql
+++ db_lab05/db_lab05.sql
@@ -64,3 +64,85 @@ where tablespace_name = 'YNK_QDATA';
 
 -- 8
 drop tablespace ynk_qdata including contents and datafiles;
+
+-- 9: все группы журналов, текуща€ - current
+select * from v$log;
+
+-- 10: файлы всех журналов
+select * from v$logfile;
+
+-- 11: пройти круг по логфайлам (после изменени€ current мен€етс€ на следующий
+-- в цикле 1 -> 2 -> 3 -> 1)
+alter system switch logfile;
+select * from v$log;
+
+-- 12: добавить группу и 3 логфайла в нее
+alter database add logfile group 4 'C:\app\oracle_user\oradata\orcl\REDO04.LOG'
+    size 50m blocksize 512;
+alter database add logfile member 
+    'C:\app\oracle_user\oradata\orcl\REDO041.LOG'  to group 4;
+alter database add logfile member 
+    'C:\app\oracle_user\oradata\orcl\REDO042.LOG'  to group 4;
+
+select * from v$log;
+select * from v$logfile;
+
+-- 13: удалить созданный лог и логфайлы
+alter database drop logfile member 'C:\app\oracle_user\oradata\orcl\REDO042.LOG';
+alter database drop logfile member 'C:\app\oracle_user\oradata\orcl\REDO041.LOG';
+alter database drop logfile group 4;
+
+select * from v$log;
+select * from v$logfile;
+
+-- 14
+select name, log_mode from v$database;
+select instance_name, archiver, active_state from v$instance;
+
+-- 15
+select * from v$log;
+select * from v$archived_log;
+
+-- 16 включить архивирование через sqlplus на сервере
+-- connect / as sysdba;
+-- shutdown immediate;
+-- startup mount;
+-- alter database archivelog;
+-- alter database open;
+select name, log_mode from v$database;
+select instance_name, archiver, active_state from v$instance;
+
+-- 17: принудительно создать архивный файл
+alter system switch logfile;
+select * from v$archived_log;
+
+-- 18: отключить архивирование (sqlplus)
+-- startup mount;
+-- alter database noarchivelog;
+select name, log_mode from v$database;
+select instance_name, archiver, active_state from v$instance;
+
+-- 19: список управл€ющих файлов
+select * from v$controlfile;
+
+-- 20:
+show parameter control;
+select * from v$controlfile_record_section;
+
+-- 21: расположение файла параментров:
+-- C:\app\oracle_user\product\12.1.0\dbhome_1\database
+
+-- 22: 
+create pfile = 'ynk_pfile.ora' from spfile;
+-- расположение: C:\app\oracle_user\product\12.1.0\dbhome_1\database\ynk_pfile.ora
+
+-- 23: расположение файла паролей инстанса:
+-- C:\app\oracle_user\product\12.1.0\dbhome_1\database\PWDorcl.ora
+select * from v$pwfile_users;
+show parameter remote_login_passwordfile;
+
+-- 24: перечень директориев дл€ файлов сообщений и диагностики
+select * from v$diag_info;
+
+-- 25: посмотреть в log.xml
+-- C:\app\oracle_user\diag\rdbms\orcl\orcl\alert\log.xml
